5761602b9565c9990da99dfa00418a3f5ac0610b
salamaashoush <salamaashoush@gmail.com>
2017-03-12 18:14:57 +0200

    deploy

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -196,6 +196,8 @@ class AuthController extends Controller
 
         $name=$response->decodeBody()['name'];
         $email=$response->decodeBody()['email'];
+        echo $email;
+        die();
         if(!empty(User::retrieveByEmail($email))){
             $user=User::retrieveByField('email',$email)[0];
             Session::saveLogin($user->username, $user->role, $user->password);
