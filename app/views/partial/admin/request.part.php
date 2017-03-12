a725e92cabe6f706da9edd41b5e1ec40d3546b24
salamaashoush <salamaashoush@gmail.com>
2017-03-12 15:39:01 +0200

    fixed bug

--- app/views/partial/admin/request.part.php
+++ app/views/partial/admin/request.part.php
@@ -4,7 +4,7 @@
         <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
                 <i class="fa fa-minus"></i></button>
-            <?php if(\App\Core\Session::getLoginUser()->id==$req->uid):?>
+            <?php if(\App\Core\Session::getLoginUser()->id==$req->uid||\App\Core\Session::getLoginUser()->role=='admin'):?>
                 <?php start_form('delete',"/requests/$req->id")?>
                 <button type="submit" class="btn btn-box-tool"  title="" data-original-title="Remove">
                     <i class="fa fa-times"></i></button>
